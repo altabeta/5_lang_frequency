ebff854469ac81ec7fe9d27e7e1f8190ac58bd74
altabeta <weilbrideru@gmail.com>
2016-12-29 04:50:04 +0300

    changed structure of code;

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,16 +1,16 @@
 import re
 import collections
-def get_filepath():
-	a = input(str())
-	return a
-def word_freq(path):
+def get_data(path):
 	starterText = open(path, 'r').read()
-	changedText = re.sub(r'[;:,\.\(\)\'\"\?\!\-\n]', ' ', starterText)
+	return starterText
+def word_freq(starterText):
+	changedText = re.sub(r'[;:,\.\(\)\'\"\?\!\-\n]', ' ', starterText) # здесь избавляемся от знаков препинания, переносов и приводим текст к удобоваримому виду;
 	changedText = changedText.lower()
 	splittedWords = re.split(r' ', changedText)
-	splittedWords = [x for x in splittedWords if x != '']
-	commonWords = collections.Counter(splittedWords).most_common(10)
+	splittedWords = [x for x in splittedWords if x != ''] # создаем список всех слов текста, попутно избавляемся от лишних пробелов; 
+	commonWords = collections.Counter(splittedWords).most_common(10) 
 	return commonWords
-a = get_filepath()
-print(word_freq(a))
+a = input(str())
+text = get_data(a)
+print(word_freq(text))
 input('Press enter to continue...')
