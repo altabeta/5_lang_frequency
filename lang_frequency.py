5cd6e93f85383e96a5d80da2293b88572c83acd3
altabeta <weilbrideru@gmail.com>
2016-12-29 04:37:09 +0300

    added necessary changes;

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,16 @@
-
-
-def load_data(filepath):
-    pass
-
-
-def get_most_frequent_words(text):
-    pass
-
-
-if __name__ == '__main__':
-    pass
+import re
+import collections
+def get_filepath():
+	a = input(str())
+	return a
+def word_freq(path):
+	starterText = open(path, 'r').read()
+	changedText = re.sub(r'[;:,\.\(\)\'\"\?\!\-\n]', ' ', starterText)
+	changedText = changedText.lower()
+	splittedWords = re.split(r' ', changedText)
+	splittedWords = [x for x in splittedWords if x != '']
+	commonWords = collections.Counter(splittedWords).most_common(10)
+	return commonWords
+a = get_filepath()
+print(word_freq(a))
+input('Press enter to continue...')
