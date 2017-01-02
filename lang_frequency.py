dddc49c29cfb6cec41447a4c566618d234018bcc
altabeta <weilbrideru@gmail.com>
2017-01-02 18:36:49 +0300

    used snake case;

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,15 +1,15 @@
 import re
 import collections
 def get_data(path):
-	starterText = open(path, 'r').read()
-	return starterText
-def word_freq(starterText):
-	changedText = re.sub(r'[;:,\.\(\)\'\"\?\!\-\n]', ' ', starterText) # swapping punctuation marks with spaces;
-	changedText = changedText.lower()
-	splittedWords = re.split(r' ', changedText)
-	splittedWords = [x for x in splittedWords if x != ''] # creating word list, deleting excess spaces; 
-	commonWords = collections.Counter(splittedWords).most_common(10) 
-	return commonWords
+	starter_text = open(path, 'r').read()
+	return starter_text
+def word_freq(starter_text):
+	changed_text = re.sub(r'[;:,\.\(\)\'\"\?\!\-\n]', ' ', starter_text) # swapping punctuation marks with spaces;
+	changed_text = changed_text.lower()
+	splitted_words = re.split(r' ', changed_text)
+	splitted_words = [x for x in splitted_words if x != ''] # creating word list, deleting excess spaces; 
+	common_words = collections.Counter(splitted_words).most_common(10) 
+	return common_words
 a = input(str())
 text = get_data(a)
 print(word_freq(text))
